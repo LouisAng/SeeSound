bc055a0692a49ab4179c222816615bc71d1be07f
louisan <ramovitabella@gmail.com>
2025-02-05 07:18:14 +0900

    Initial Commit

---
+++ SeeSound/SeeSoundApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeeSoundApp.swift
+//  SeeSound
+//
+//  Created by Louis An on 2/5/25.
+//
+
+import SwiftUI
+
+@main
+struct SeeSoundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
